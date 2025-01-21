d8d1296ac60a2094071352241c5615639c7c1535
magicpig <weihongjie@bytetrade.io>
2025-01-21 08:04:48 +0000

    fix: correct Special token config data structure

--- src/data_config.ts
+++ src/data_config.ts
@@ -257,10 +257,13 @@ class DataConfig {
         "🔄 Awaiting valid hedge account configuration..."
       );
     }
-    const specialTokens = _.get(baseConfig, "specialTokenConfig.fixedPriceTokens", []);
+    const specialTokens = _.get(baseConfig, "specialTokenConfig.orderBookConfig", []);
     this.specialTokenConfig.clear();
     for (const token of specialTokens) {
-      this.specialTokenConfig.set(token.symbol, token);
+      this.specialTokenConfig.set(token.symbol, {
+        symbol:token.symbol,
+        orderBookConfig:token,
+      });
     }
   }
   public getSpecialTokenConfig(symbol: string): ISpecialTokenConfig | undefined {
